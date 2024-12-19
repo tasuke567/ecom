d0c511460f9b0fd4d0b190019109fc1f611d4e5b
tasuke567 <80236261+tasuke567@users.noreply.github.com>
2024-12-19 15:33:13 +0700

    ss

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const authRouter = require('../routes/auth');
+const authRouter = require('./routes/auth/auth');
 require('dotenv').config();
 
 const app = express();
