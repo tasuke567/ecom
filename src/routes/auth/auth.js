b5fcf7d0c71be98fd990913edafb47d05260c01e
tasuke567 <80236261+tasuke567@users.noreply.github.com>
2024-12-24 11:10:07 +0700

    Update auth.js

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
-const client = require('../config/google');
+const client = require('../../config/google');
 const bcrypt = require('bcryptjs');
 
 
